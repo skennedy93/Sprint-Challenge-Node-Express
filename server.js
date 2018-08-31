717ca483448d6fda161af665b63e695be63efe50
Sean <sean47900@gmail.com>
2018-08-31 12:04:23 -0500

    PUT method done

--- server.js
+++ server.js
@@ -23,7 +23,6 @@ server.get('/', (req, res) => {
 server.get('/projects/:id', (req, res) => {
     dbProject.get(req.params.id)
     .then(project => {
-        // console.log(project);
         if (!project) {
             res.status(404).json({ message: 'The project with that ID does not exist.' });
             return;
@@ -67,7 +66,25 @@ server.delete('/projects/:id', (req, res) => {
                 res.status(404).json({ message: 'The project with that ID does not exist.' })
             }
         })
-        .catch(error => res.status(500).json({ error: 'The project could not be removed' }));
+        .catch(error => res.status(500).json({ error: 'The project could not be deleted' }));
+});
+
+server.put('/projects/:id', (req, res) => {
+    const { name, description } = req.body;
+    if (!name || !description) {
+        res.status(400).json({ errorMessage: 'Name and description required' });
+        return;
+    }
+    dbProject.update(req.params.id, req.body)
+        .then(project => {
+            if (project) {
+                res.status(200).json(req.body)
+            } else {
+                res.status(404).json({ message: 'The project with that ID does not exist.' })
+            }
+            
+        })
+        .catch(err => res.status(500).json({ message: 'The project could not be updated.' }));
 });
 
  server.listen(3000, () => console.log('/n== API on port 3000 ==/n') );
